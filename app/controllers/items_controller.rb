d8345282544815eb1d3d464360ca91e8ce20f1df
miyaichi <burnedsoba@gmail.com>
2022-02-23 13:45:09 +0900

    [Add]ひとまず1件ずつ登録できるようにした

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,25 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+  
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:code, :name, :price)
+  end
+end
